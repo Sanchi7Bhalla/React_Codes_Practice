26ca72e35e72496e665f7cd88c32b2a72ccaad93
sanchi <sanchibhalla98@gmail.com>
2020-12-26 20:17:01 +0530

    Practice_2

--- daily-task-manager-app/src/App.js
+++ daily-task-manager-app/src/App.js
@@ -4,22 +4,75 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList.js";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [isErr, setIsErr] = useState(false);
+  useEffect(() => {
+    // Runs only once when app starts
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "uncompleted":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    // Saving to local
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // Checking in local
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>TASK MANAGER</h1>
       </header>
+      {/* <p>
+        <em>Plan less. Do more!</em>
+      </p> */}
+
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+        setIsErr={setIsErr}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
+      <p>
+        <em>{isErr ? "Input is empty" : ""}</em>
+      </p>
     </div>
   );
 }
